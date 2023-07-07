3e0a6ba87dd15cc0ddd9230c230e489bec00b3b4
Laiza <110543359+Zaila07@users.noreply.github.com>
2023-07-07 14:16:20 +0800

    Add files via upload

---
+++ Reimbursement.Business/DataTransferObject/MonthlyReimbursementDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprEmployeeReimbursement.Business.DataTransferObject
+{
+    public class MonthlyReimbursementDto
+    {
+        public string EmployeeName { get;set; }
+        public decimal? MonthlyTotal { get; set; }
+
+    }
+}
---
+++ Reimbursement.Business/Helper/FormRecognizerHelper.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer;
+using Microsoft.AspNetCore.Http;
+
+
+namespace SprEmployeeReimbursement.Business.FormRecognizer
+{
+    public class FormRecognizerHelper
+    {
+        public static async Task<string> ExtractReceiptTextAsync(IFormFile receiptImage, string apiKey, string endpoint)
+        {
+            var formRecognizerOptions = new AzureKeyCredential(apiKey);
+            var formRecognizerClient = new FormRecognizerClient(new Uri(endpoint), formRecognizerOptions);
+          
+
+           
+           
+                using var stream = receiptImage.OpenReadStream();
+                var form = await formRecognizerClient.StartRecognizeContentAsync(stream);
+                var operationResult = await form.WaitForCompletionAsync();
+                var formPage = operationResult.Value.FirstOrDefault();
+
+                if (formPage != null)
+                {
+                    var receiptText = string.Join(" ", formPage.Lines
+                        .Select(line => line.Text));
+                  return receiptText;
+                }
+
+
+            return string.Empty;
+        }
+    }
+}
