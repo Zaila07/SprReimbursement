a9be384c7e3e9b6056084421db33ba5d935c98ca
Laiza Zuniga <ayahmoscosa12@gmail.com>
2023-07-31 11:00:47 +0800

    EmployeesController,EmployeeServices,DTOs,
    Adding EmployeesController,Services,DTOs

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SprEmployeeReimbursement.Business.DataTransferObject;
+using SprEmployeeReimbursement.Business.ServiceCollection;
+using SprEmployeeReimbursement.DataAccess.Models;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace SprEmployeeReimbursement.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:ApiVersion}/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IConfiguration configuration, IEmployeeService employeeService)
+        {
+            _configuration = configuration;
+            _employeeService = employeeService;
+        }
+        [ProducesResponseType(typeof(CreateEmployeeDto), statusCode: StatusCodes.Status201Created)]
+        [MapToApiVersion("1")]
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] CreateEmployeeDto createEmployeeDto)
+        {
+            var createdEmployee = await _employeeService.CreateEmployee(createEmployeeDto);
+            return Created($"api/v1/Employees/{createdEmployee}", createdEmployee);
+        }
+
+        [ProducesResponseType(typeof(SprEmployee), statusCode: StatusCodes.Status200OK)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult>GetEmployeeByID(int id)
+        {
+            return Ok(await _employeeService.GetEmployeeById(id));
+
+        }
+        [ProducesResponseType(typeof(employeeDto), statusCode: StatusCodes.Status200OK)]
+        [HttpGet] 
+        public async Task<IActionResult> GetAllEmployees()
+        {
+
+            return Ok( await _employeeService.GetAllEmployees());
+        }
+
+        [ProducesResponseType(typeof(List<string>), statusCode: StatusCodes.Status200OK)]
+        [HttpGet("names")]
+        public async Task<IActionResult> GetEmployeeNames()
+        { 
+           return Ok (await _employeeService.GetEmployeeNames());
+        }
+
+
+        [ProducesResponseType(typeof(UpdateEmployeeDto),statusCode:StatusCodes.Status200OK)]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee( [FromBody] UpdateEmployeeDto updateEmployeeDto)
+        { 
+            return Ok(await _employeeService.UpdateEmployee(updateEmployeeDto));
+        }
+        [ProducesResponseType(typeof(bool),statusCode:StatusCodes.Status200OK)]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        { 
+            return Ok(await _employeeService.DeleteEmployee(id));
+        
+        }
+    }
+ 
+    }
+
+
--- Controllers/ReimbursementsController.cs
+++ Controllers/ReimbursementsController.cs
@@ -5,23 +5,23 @@ using SprEmployeeReimbursement.Business.DataTransferObject;
 using SprEmployeeReimbursement.Business.ServiceCollection;
 using SprEmployeeReimbursement.DataAccess.SprDbContext;
 using SprEmployeeReimbursement.DataAccess.Models;
-using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Logging;
 
 namespace SprEmployeeReimbursement.Controllers
 {
     [ApiVersion("1")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
-    public class ReimbursementController : ControllerBase
+    public class ReimbursementsController : ControllerBase
     {
         private readonly IConfiguration _configuration;
         private readonly IReimbursementService _reimbursementService;
         private readonly FormRecognizerClient _formRecognizerClient;
         private readonly SprReimbursementDbContext _context;
-        private readonly ILogger<ReimbursementController> _logger;
+        private readonly ILogger<ReimbursementsController> _logger;
 
-        public ReimbursementController(IConfiguration configuration, FormRecognizerClient formRecognizerClient,
-            SprReimbursementDbContext context, IReimbursementService reimbursementService, ILogger<ReimbursementController> logger)
+        public ReimbursementsController(IConfiguration configuration, FormRecognizerClient formRecognizerClient,
+            SprReimbursementDbContext context, IReimbursementService reimbursementService, ILogger<ReimbursementsController> logger)
         {
             _context = context;
             _configuration = configuration;
@@ -35,19 +35,34 @@ namespace SprEmployeeReimbursement.Controllers
         [HttpPost]
         public async Task<IActionResult> ProcessMultipleReceiptsReimbursement([FromForm] MultipleReceiptsReimbursementDto input)
         {
+            // Log the request payload for debugging purposes
+            LogRequestPayload(input);
+
             var result = await _reimbursementService.ProcessMultipleReceiptsReimbursement(input);
             return Created($"api/v1/Reimbursement/{result}", result);
+
+            // Helper method to log the request payload
+             void LogRequestPayload(MultipleReceiptsReimbursementDto input)
+            {
+                _logger.LogInformation("Received request payload:");
+                _logger.LogInformation($"EmployeeName: {input.EmployeeName}");
+                _logger.LogInformation($"EmployeeId: {input.EmployeeId}");
+                _logger.LogInformation($"Type: {input.Type}");
+                _logger.LogInformation($"TransactionDate: {input.TransactionDate}");
+                _logger.LogInformation($"Number of receipt images: {input.ReceiptImages?.Count}");
+            }
+
         }
 
         [ProducesResponseType(typeof(ReimbursementModel), statusCode: StatusCodes.Status200OK)]
-        [HttpGet("hr/{id}")]
-        public async Task<IActionResult> GetReimbursementForHR(int id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReimbursementById(int id)
         {
             return Ok(await _context.ReimbursementModels.FindAsync(id));
         }
 
         [ProducesResponseType(typeof(List<ReimbursementModel>), statusCode: StatusCodes.Status200OK)]
-        [HttpGet("All")]
+        [HttpGet]
         public async Task<IActionResult> GetAllReimbursements()
         {
             return Ok(await _reimbursementService.GetAllReimbursements());
@@ -69,14 +84,14 @@ namespace SprEmployeeReimbursement.Controllers
             return Ok(deletedReimbursement);
         }
 
-        [HttpPost("hr/approve/{id}")]
+        [HttpPost("approve/{id}")]
         [ProducesResponseType(typeof(ReimbursementModel), statusCode: StatusCodes.Status200OK)]
         public async Task<IActionResult>ApproveReimbursement(int id)
         {
             var reimbursement = await _reimbursementService.ApproveReimbursement(id);
             return Ok(reimbursement);
         }
-        [HttpPost("hr/disapprove/{id}")]
+        [HttpPost("disapprove/{id}")]
         [ProducesResponseType(typeof(ReimbursementModel), statusCode: StatusCodes.Status200OK)]
         public async Task<IActionResult> DisapproveReimbursement(int id, [FromBody] string reason)
         {
@@ -84,7 +99,7 @@ namespace SprEmployeeReimbursement.Controllers
             return Ok(reimbursement);
 
         }
-        [HttpGet("hr/total/employee/reimbursements/{id}")]
+        [HttpGet("/total/employee/reimbursements/{id}")]
         [ProducesResponseType(typeof(MonthlyReimbursementDto),statusCode:StatusCodes.Status200OK)]
         public async Task<IActionResult> GetTotalMonthlyReimbursement(string id)
         {
